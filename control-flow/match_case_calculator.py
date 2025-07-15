33eb824a7d28ec3eed7229636d98130d951f33d1
Christiana <christine.anako@gmail.com>
2025-07-15 14:30:12 -0400

    Calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,13 +2,15 @@ first = int(input("Enter the first number: "))
 second = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
-    print(f"The result is {first + second}.")
-elif operation == "-":
-    print(f"The result is {first - second}.")
-elif operation == "*":
-    print(f"The result is {first * second}.")
-elif operation == "/" and second != 0:
-    print(f"The result is {first / second}.")
-elif operation == "/" and second == 0:
-    print(f"Cannot divide by zero.")
+match operation:
+    case "+":
+        print(f"The result is {first + second}.")
+    case "-":
+        print(f"The result is {first - second}.")
+    case "*":
+        print(f"The result is {first * second}.")
+    case "/":
+        if second != 0:
+            print(f"The result is {first / second}.")
+        else:
+            print("Cannot divide by zero.")
