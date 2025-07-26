7f02072f24429da133e53fdc6bb87b9df7e7cb9e
Christiana <christine.anako@gmail.com>
2025-07-26 22:45:57 +0100

    Functions

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,26 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            value = input("Enter a value to add ")
+            shopping_list.append(value)
+        elif choice == '2':
+            value = input("Enter a value to remove ")
+            shopping_list.remove(value)
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
